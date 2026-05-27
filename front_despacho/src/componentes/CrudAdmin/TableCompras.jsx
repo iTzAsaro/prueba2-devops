5e440f7a09208914f64351c37e9874329e94ca48
iTzAsaro <alexsander.rosales33@gmail.com>
2026-05-26 22:58:46 -0400

    corregido

--- front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableCompras.jsx
@@ -7,11 +7,11 @@ export const TableCompras = () => {
   const [ventas, setVentas] = useState([]);
 
   const compras = async () => {
-    await axios.get("http://192.168.30/api/v1/ventas", {
-      headers:{
+    await axios.get("/api/v1/ventas", {
+      headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
-  }
+      }
     }).then((response) => {
       console.log(response.data);
       setVentas(response.data);
